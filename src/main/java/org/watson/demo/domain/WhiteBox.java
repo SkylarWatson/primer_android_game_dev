e0588a0eddd021f01fe1faf575f6e99385e35fe2
Skylar Watson <skylarwatson@Skylar-Watsons-MacBook-Pro.local>
2011-10-23 21:54:21 -0500

    Skylar: Removed unused code.

--- src/main/java/org/watson/demo/domain/WhiteBox.java
+++ src/main/java/org/watson/demo/domain/WhiteBox.java
@@ -1,7 +1,5 @@
 package org.watson.demo.domain;
 
-import org.anddev.andengine.engine.handler.IUpdateHandler;
-import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
 
@@ -14,22 +12,5 @@ public class WhiteBox {
 
     public Sprite asSprite() {
         return sprite;
-    }
-
-    public void move(final Scene scene, final float width) {
-        scene.registerUpdateHandler(new IUpdateHandler() {
-            @Override
-            public void onUpdate(float pSecondsElapsed) {
-                if(width >= sprite.getX() + sprite.getWidth()) {
-                    sprite.setPosition(sprite.getX() + 0.5f, sprite.getY());
-                } else {
-                    scene.unregisterUpdateHandler(this);
-                }
-            }
-
-            @Override
-            public void reset() {
-            }
-        });
     }
 }
